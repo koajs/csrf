e35d643213a0fee2f1389740d57939970986239b
popomore <sakura9515@gmail.com>
2015-10-28 21:02:51 +0800

    More infomation when missing secret or token

--- test/csrf.test.js
+++ test/csrf.test.js
@@ -1,4 +1,5 @@
 var supertest = require('supertest')
+var should = require('should');
 var koa = require('koa')
 var sessions = require('koa-session')
 var parse = require('co-body')
@@ -21,7 +22,7 @@ describe('CSRF Token', function () {
         body = yield parse(this)
       } catch (err) {}
       if (this.path === '/string') this.assertCSRF(body._csrf)
-      else this.assertCSRF(body)
+      else this.request.assertCSRF(body)
       this.status = 204
     }
   })
@@ -47,7 +48,7 @@ describe('CSRF Token', function () {
 
     it('a single token per request', function (done) {
       app.use(function* (next) {
-        this.csrf.should.equal(this.csrf)
+        this.response.csrf.should.equal(this.csrf)
         this.status = 204
       })
 
@@ -75,7 +76,34 @@ describe('CSRF Token', function () {
     it('when no token is supplied', function (done) {
       request
       .post('/')
-      .expect(403, done)
+      .expect(403, function(err, res) {
+        should.not.exists(err)
+        res.text.should.eql('token is missing')
+        done()
+      })
+    })
+
+    it('when no secret is supplied', function (done) {
+      supertest(app.listen())
+      .post('/')
+      .expect(403, function(err, res) {
+        should.not.exists(err)
+        res.text.should.eql('secret is missing')
+        done()
+      })
+    })
+
+    it('when invalid csrf token', function (done) {
+      request
+      .post('/')
+      .send({
+        _csrf: 'wrong csrf token'
+      })
+      .expect(403, function(err, res) {
+        should.not.exists(err)
+        res.text.should.eql('invalid csrf token')
+        done()
+      })
     })
   })
 
