e66221fb18528202603a0446eeff58b075f3de95
teawithfruit <teawithfruit@gmail.com>
2020-05-06 14:33:32 +0200

    Prettier code

--- src/index.js
+++ src/index.js
@@ -43,10 +43,11 @@ class CSRF {
       return next();
     }
 
-    if (this.opts.excludedHosts.some(x => x.test(ctx.request.ip)) || this.opts.excludedHosts.some(x => x.test(ctx.request.host)) ) {
+    if (this.opts.excludedHosts.some(x => x.test(ctx.request.ip)) ||
+        this.opts.excludedHosts.some(x => x.test(ctx.request.host))) {
       return next();
     }
-    
+
     if (!ctx.session.secret) {
       ctx.session.secret = this.tokens.secretSync();
     }
